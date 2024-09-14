a896009b0d8526ae55f749798aeef35cb993b2d2
hibiki.suzuki <hbear1029@outlook.jp>
2024-09-14 11:45:20 +0900

    Lesson16課題

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 //Lesson 16Chapter 4.6「コントローラーの作成」
 @Controller
-@RequestMapping("country")
+@RequestMapping("/country")
 public class CountryController {
 	private final CountryService service;
 
@@ -29,7 +29,7 @@ public class CountryController {
 
 	//Lesson16 Chapter5.2追加
 	//詳細画面
-	@GetMapping(value = {"/detail", "/detail/{code}/"})//新規登録と更新2つ分パスのマッピング
+	@GetMapping(value = {"/detail", "/detail/{code}"})//新規登録と更新2つ分パスのマッピング
 	public String getCountry(@PathVariable(name = "code", required = false)String code, Model model) {
 		//codeが指定されていたら検索結果、無ければ空のクラスを設定
 		Country country = code != null ? service.getCountry(code): new Country();
@@ -51,6 +51,12 @@ public class CountryController {
 	//削除画面
 	@GetMapping("/delete")
 	public String deleteCountryForm(Model model) {
+		return "country/delete";
+	}
+	@GetMapping("/delete/{code}") //課題
+	public String getCountry1(@PathVariable("code")String code, Model model) {
+		Country country = service.getCountry(code);
+		model.addAttribute("country", country);
 		return "country/delete";
 	}
 
